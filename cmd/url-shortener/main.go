0c992a4f61e150ac0b0cd758b0320768139a5826
Aleksandr Tumanov <tumalexx02@gmail.com>
2025-01-15 20:35:23 +0500

    Create slog logger and config loading

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,42 @@
 package main
 
-func main() {
-	// TODO: cfg init
+import (
+	"log/slog"
+	"os"
+	"url-shortner/internal/config"
+)
 
-	// TODO: logger init
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoadConfig()
+
+	log := initLogger(cfg.Env)
+
+	log.Info("starting server", slog.String("env", cfg.Env))
 
 	// TODO: db init
 
 	// TODO: router init
 
 	// TODO: run server
+}
+
+func initLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
