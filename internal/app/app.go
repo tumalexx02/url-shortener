1988449c3b8a2226c2c9728a996f5a0d6f74323a
Aleksandr Tumanov <tumalexx02@gmail.com>
2025-01-29 00:59:18 +0500

    Delete Jobs struct and Jobs scheduler; Create dailyRatePeakReset method

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,18 +1,15 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	cr "github.com/robfig/cron/v3"
 	"log/slog"
 	"net/http"
-	"strconv"
 	"time"
 	"url-shortner/internal/config"
 	mwLogger "url-shortner/internal/http-server/middleware/logger"
-	"url-shortner/internal/jobs"
-	"url-shortner/internal/jobs/reset-peak-rate"
 	rl "url-shortner/internal/rate-limiter"
 	"url-shortner/internal/routes"
 	"url-shortner/internal/storage/postgres"
@@ -23,7 +20,7 @@ type App struct {
 	log         *slog.Logger
 	storage     *postgres.Storage
 	router      *chi.Mux
-	ratelimiter *rl.RateLimiter
+	rateLimiter *rl.RateLimiter
 }
 
 func New(cfg *config.Config, log *slog.Logger) (*App, error) {
@@ -57,8 +54,10 @@ func New(cfg *config.Config, log *slog.Logger) (*App, error) {
 func (a *App) Start() error {
 	const op = "app.Start"
 
-	resetPeakJob := reset_peak_rate.New(a.log, a.ratelimiter)
-	err := a.startJobsScheduler("0 0 * * *", resetPeakJob)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	defer cancelCtx()
+
+	err := a.startDailyPeakReset(ctx)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
@@ -76,27 +75,36 @@ func (a *App) Start() error {
 	return srv.ListenAndServe()
 }
 
-func (a *App) startJobsScheduler(cronPattern string, jobs ...*jobs.Job) error {
-	const op = "app.startJobsScheduler"
+func (a *App) startDailyPeakReset(ctx context.Context) error {
+	const op = "app.startDailyJobs"
 
 	loc, err := time.LoadLocation(a.cfg.Location)
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
 
-	c := cr.New(cr.WithLocation(loc))
+	a.log.Info("starting daily peak reset", slog.String("location", a.cfg.Location))
 
-	for _, job := range jobs {
-		_, err = c.AddJob(cronPattern, job)
-		if err != nil {
-			return fmt.Errorf("%s: %w", op, err)
+	go func() {
+		for {
+			now := time.Now().In(loc)
+			next := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, loc)
+			duration := time.Until(next)
+
+			timer := time.NewTimer(duration)
+
+			select {
+			case <-ctx.Done():
+				timer.Stop()
+				a.log.Info("stopping daily peak reset")
+				return
+			case <-timer.C:
+				lastPeakRate := a.rateLimiter.GetPeakRate()
+				a.rateLimiter.ResetPeakRate()
+				a.log.Info("peak rate reset completed", slog.Int("last-peak-rate", lastPeakRate))
+			}
 		}
-		a.log.Info("job added", slog.String("job", job.Name))
-	}
-
-	c.Start()
-
-	a.log.Info("jobs scheduler enabled", slog.String("jobs", strconv.Itoa(len(jobs))))
+	}()
 
 	return nil
 }
--- internal/jobs/jobs.go
+++
@@ -1,8 +0,0 @@
-package jobs
-
-import "github.com/robfig/cron/v3"
-
-type Job struct {
-	Name string
-	cron.Job
-}
